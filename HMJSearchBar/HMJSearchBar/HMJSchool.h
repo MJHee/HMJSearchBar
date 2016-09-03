c8bae8ad6996512f6992341e5c9ca475dbdc4cbe
贺梦洁 <2372621687@qq.com>
2016-09-03 17:14:55 +0800

    添加学校模型

---
+++ HMJSearchBar/HMJSearchBar/HMJSchool.h
@@ -0,0 +1,16 @@
+//
+//  HMJSchool.h
+//  HMJSearchBar
+//
+//  Created by MJHee on 16/9/2.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMJSchool : NSObject
+
+@property (nonatomic, assign) int ID;
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ HMJSearchBar/HMJSearchBar/HMJSchool.m
@@ -0,0 +1,13 @@
+//
+//  HMJSchool.m
+//  HMJSearchBar
+//
+//  Created by MJHee on 16/9/2.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import "HMJSchool.h"
+
+@implementation HMJSchool
+
+@end
