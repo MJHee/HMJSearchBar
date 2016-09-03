17fb03bdc3f226e22587ac10ea31d97767315d14
贺梦洁 <2372621687@qq.com>
2016-09-03 17:19:56 +0800

    实现页面跳转

--- HMJSearchBar/HMJSearchBar/ViewController.m
+++ HMJSearchBar/HMJSearchBar/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "HMJSearchViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UISearchBarDelegate>
+@property (nonatomic, strong) UIView *titleView;
+@property (nonatomic, strong) UIButton *pushBtn;
 
 @end
 
@@ -16,12 +19,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    self.navigationItem.titleView = self.titleView;
+
+    [self.view addSubview:self.pushBtn];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - pushToController
+
+- (void)pushToController:(UIButton *)sender {
+    [self pushToSearchViewController];
+}
+
+#pragma mark - 懒加载
+
+- (UIButton *)pushBtn {
+    if (_pushBtn == nil) {
+        _pushBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _pushBtn.backgroundColor = [UIColor redColor];
+        _pushBtn.frame = CGRectMake(kScreenW - 100, 20, 80, 30);
+        [_pushBtn setTitle:@"跳转" forState:UIControlStateNormal];
+        [_pushBtn addTarget:self action:@selector(pushToController:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _pushBtn;
+}
+
+- (UIView *)titleView {
+
+    if (_titleView == nil) {
+        _titleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 28)];
+        UIColor *color = self.navigationController.navigationBar.backgroundColor;
+        _titleView.backgroundColor = color;
+        UISearchBar *searchBar = [[UISearchBar alloc] init];
+        searchBar.frame = CGRectMake(0, 0, 300, 28);
+        searchBar.backgroundColor = color;
+        searchBar.delegate = self;
+        searchBar.layer.cornerRadius  = 14;
+        searchBar.layer.masksToBounds = YES;
+        searchBar.keyboardType = UIKeyboardTypeDefault;
+        searchBar.placeholder  = @"输入中文、英文名或分子式";
+        [_titleView addSubview:searchBar];
+    }
+    return _titleView;
 }
 
 @end
